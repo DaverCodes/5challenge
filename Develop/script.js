8a2ed65e0b64297ffd5481844ad6be5401fbfe1e
daveS <davesimpcontrol@gmail.com>
2023-01-27 21:41:51 -0700

    added working time to code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var today = dayjs();
+$('#1a').text(today.format('MMM DD, YYYY [at] hh:mm:ss a'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,6 +25,7 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
 
 
 
