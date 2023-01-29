ba8ead666155a2aa307b66d26c70c73b99420741
daveS <davesimpcontrol@gmail.com>
2023-01-29 14:37:27 -0700

    moved things around

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,21 @@
 
 const textZones = [...document.querySelectorAll('.col-8')];
 const buttons = [...document.querySelectorAll('.btn')];
-const textarray = document.getElementById('.9text')
-const hourArray = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
-console.log(hourArray);
+// const textarray = document.getElementById('.9text')
+// const hourArray = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
+// console.log(hourArray);
 
 buttons.forEach(function(buttonArrayFinder) {
   buttonArrayFinder.addEventListener('click', function() {
     let saveArrayPoint = buttons.indexOf(this);
     console.log(saveArrayPoint);
     let saveText = textZones[saveArrayPoint].value
+    let logHr = saveArrayPoint + 9 
+    console.log(logHr, saveText);
+    localStorage.setItem(logHr, saveText)
+
     if (saveArrayPoint <= 11) {
-      let logHr = saveArrayPoint + 9 + 'AM'
+      let logHr = saveArrayPoint + 9 
       console.log(logHr, saveText);
       localStorage.setItem(logHr, saveText)
     } else {
@@ -29,7 +33,8 @@ buttons.forEach(function(buttonArrayFinder) {
     }
   });
 });
-// get item and put it in text area
+// WHEN I refresh the page
+// THEN the saved events persist= local storage
 var keyList = Object.keys(localStorage)
 console.log(keyList);
 
@@ -44,48 +49,21 @@ if (hourArray.includes(keyList)) {
 let 
 console.log(localStorage.getItem("9am"));
 
+
+
+
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar = add day.js
 var today = dayjs();
 $('#currentDay').text(today.format('MMM DD, YYYY [at] HH:mm:ss:SSS'));
 
-// save.onclick = function () {
-//   const log9 = hrInput9.value;
-  
-//   console.log(log9);
-//   localStorage.setItem('9am', log9);
-//   location.reload();
-
-
-
-// };
-
-
-// const timeContinuum = dayjs().hour()
-// const workArray = 
-
-//options for past-present-future color code
 
 
 
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+
 });
 
 // javascript code that knows if a number between 9 and 17 is greater than- less thab- or equal to any other number in that array
@@ -100,8 +78,6 @@ $(function () {
 // GIVEN I am using a daily planner to create a schedule
 
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar = add day.js
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours=finish up time blocks
@@ -114,6 +90,3 @@ $(function () {
 
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist= local storage
